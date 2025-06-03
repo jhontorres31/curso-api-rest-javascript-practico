1154f4594cd61cb65356e0c1676b4d2fbadb2dd5
jhontorres31 <jftc10@gmail.com>
2025-06-03 08:39:13 -0500

    hash

--- cod.js
+++ cod.js
@@ -1,12 +1,23 @@
-const api = axios.create();
+const api = axios.create({
+
+  baseURL: 'https://api.themoviedb.org/3/',
+  Headers:{
+    'Content-type':'application/json;charset=utf-8'
+  },
+  params: {
+
+    'api_key':API_KEY,
+
+  },
+
+});
+
 
 async function getTrendingMoviesPreview(){
 
-    const res = await fetch ('https://api.themoviedb.org/3/trending/movie/day?api_key='+API_KEY);
-    const data = await res.json();
-
+    const { data } = await api ('trending/movie/day');
     const movies = data.results;
-   // console.log({data, movies});
+   
     
      movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
@@ -23,9 +34,7 @@ async function getTrendingMoviesPreview(){
     });
 }
 async function getCategoriesPreview(){
-    const res = await fetch ('https://api.themoviedb.org/3/genre/movie/list?api_key='+API_KEY);
-    const data = await res.json();
-
+    const {data} = await api ('genre/movie/list');
     const categories = data.genres;
    
     
@@ -55,7 +64,6 @@ async function getCategoriesPreview(){
     
 }
 
-getTrendingMoviesPreview();
-getCategoriesPreview();
+
 
 
